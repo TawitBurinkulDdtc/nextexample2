3a1d391c98a8121f6a2c3a2e5df5ff5eb98a8740
TawitBurinkulDdtc <141985513+TawitBurinkulDdtc@users.noreply.github.com>
2025-10-08 18:29:23 +0700

    Whyyyy!?!?

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           </li>
         </ol>
         <div className="flex flex-col items-center text-center px-4 py-12">
-          oh yeah, update work, now test again
+          oh what
         </div>
 
         <h1 className="text-5xl font-extrabold text-sky-900 mb-4">
