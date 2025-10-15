cdabc48e616f367f0a42bd00300595e12976b5bf
TawitBurinkulDdtc <141985513+TawitBurinkulDdtc@users.noreply.github.com>
2025-10-15 14:29:14 +0700

    Brink bakc words

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          {/* <li className="mb-2 tracking-[-.01em]">
+           <li className="mb-2 tracking-[-.01em]">
             Hey Zen is here. Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
             .
-          </li> */}
+          </li> 
           <li className="tracking-[-.01em]">
             Game search engine web
           </li>
