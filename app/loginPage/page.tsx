a2bf3b19461372e722c6097a96fedf4a557c033d
TawitBurinkulDdtc <141985513+TawitBurinkulDdtc@users.noreply.github.com>
2025-12-06 16:38:14 +0700

    I can't open folder without commit, idk how to solve this.

--- app/loginPage/page.tsx
+++ app/loginPage/page.tsx
@@ -26,60 +26,16 @@ export default function Login() {
         revalidatePath('/','layout');
         return redirect('/profile');
     }
-    const signup = async (formData: FormData) => {
-        'use server';
-        const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
-
-        const cookieStore = await cookies();
-        const supabase = createClient(cookieStore);
-
-        const data = {
-            email: email,
-            password: password,
-        }
-        const { data: uData, error } = await supabase.auth.signUp(data);
-
-        if (error) {
-            console.log("Error signing up: ", error);
-            return redirect(`/error?message=${error.message}`);
-        }
-        console.log("sign up successfully");
-
-        if (uData.user) {
-            const name = uData.user.email?.split('@')[0] || 'New User';
-            const today = new Date();
-            const membershipStart = today.toISOString().split('T')[0];
-            const twoYear = new Date();
-            twoYear.setFullYear(today.getFullYear() + 2);
-            const membershipEnd = twoYear.toISOString().split('T')[0];
-            console.log("Adding Patron");
-            const { error: insertError } = await supabase.from('patron').insert([{
-                userid: uData.user.id,
-                email: uData.user.email,
-                name: name,
-                patrontype: 'student',
-                membershipstart: membershipStart,
-                membershipend: membershipEnd
-            }]);
-            if (insertError) {
-                console.log("Error adding a patron: ", insertError);
-                return redirect(`/error?message=${insertError.message}`);
-            }
-        }
-        revalidatePath('/','layout');
-        return redirect('/profile');
-    }
+    
 
     return (
         <section className="container py-12">
             <form className="bg-white p-6 rounded-md max-w-md mx-auto">
-                <h2 className="text-xl font-bold mb-4">Register</h2>
+                <h2 className="text-xl font-bold mb-4">Login</h2>
                 <input type="email" name='email' placeholder="Email" className="w-full border p-2 mb-3 rounded" required />
                 <input type="password" name='password' placeholder="Password" className="w-full border p-2 mb-3 rounded" required />
                 <div className='flex justify-between'>
-                    <button formAction={login}  className="bg-ddct-orange text-white py-2 px-4 rounded">Log in</button>
-                    <button formAction={signup} className="bg-blue-500 text-white py-2 px-4 rounded">Sign Up</button>
+                    <button formAction={login}  className="bg-blue-500 text-white py-2 px-4 rounded">Log in</button>
                 </div>
             </form>
         </section>
